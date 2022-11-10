bc225bf889bf5449e7b0d5e48a598d2b19c0612f
Pandu Supriyono <pandu@uncinc.nl>
2022-11-10 08:57:38 +0100

    add additional keybindings for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,3 +3,7 @@ require('telescope').setup{}
 map('n', '<Leader>ff', ':Telescope find_files<CR>')
 map('n', '<Leader>fg', ':Telescope live_grep<CR>')
 map('n', '<Leader>fb', ':Telescope buffers<CR>')
+map('n', '<Leader>fh', ':Telescope oldfiles<CR>')
+map('n', '<Leader>fbr', ':Telescope git_branches<CR>')
+map('n', '<Leader>fbc', ':Telescope git_bcommits<CR>')
+map('n', '<Leader>fc', ':Telescope git_commits<CR>')
